bfac6b6e8af69d4d19a0ea664a7c0e2090037d95
Ben Bartling <ben.bartling@gmail.com>
2024-01-30 12:37:15 -0600

    bump to version v0.1.1 update to include logging computer metrics on Linux device and updated readme

--- README.md
+++ README.md
@@ -1,4 +1,18 @@
 # bacnet-csv-logger
-* BACnet CSV Logger is an asynchronous Python application designed to run on a Linux and continuously scrape data on an interval from a BACnet system within a building. It compiles this data into daily CSV files.
-* In addition to data collection, the app includes functionality to securely transfer these CSV files daily via SCP over a VPN to a SharePoint drive. This ensures centralized and secure data storage.
-* The process involves saving a new CSV file every day on the BACnet system. Subsequently, the previous day's data is transferred, undergoing basic checks and processing before synchronization with SharePoint.
+
+1. **Dual-Script System with Platform-Specific Functions**:
+   - **Linux-Based Data Scraping Script**: This script is tailored to run on a Linux system within the building's Operational Technology (OT) LAN. It is specifically designed for connectivity with the Building Automation System (BAS). Its primary function is to continuously scrape data from the BACnet system, leveraging asynchronous operations for flexibility and efficiency. The script can adjust its data scraping rate (throttling) based on the device configurations within the BAS.
+   - **Windows-Based Data Retrieval Script**: In contrast, a separate script is crafted to operate on a remote Windows machine. This script is integrated into the organization's SharePoint system for project data synchronization. Its main purpose is to handle the secure retrieval of data from the Linux machine.
+
+2. **Asynchronous and Throttled Data Collection on Linux**:
+   - The Linux script continuously collects data from the BACnet system in an asynchronous manner. This approach ensures minimal impact on system resources and allows for adaptable performance based on the BAS's current load and device settings.
+   - Data is systematically archived into daily CSV files, ensuring organized and timely storage of information.
+
+3. **Daily Data Transfer via Windows Script**:
+   - The Windows script is programmed to run once a day. Its primary task is to securely connect to the Linux machine and retrieve the archived CSV files.
+   - The transfer of data is facilitated using SCP (Secure Copy Protocol) over a VPN, ensuring a high level of security during the data transmission process.
+   - Post-transfer, the data is synchronized with the organization’s SharePoint system, facilitating centralized and secure storage of the building's automation data.
+
+4. **Efficient and Secure Workflow**:
+   - This dual-script setup enables an efficient workflow where continuous, real-time data collection is handled by the Linux system, and secure, daily data transfer and synchronization are managed by the Windows system.
+   - The division of tasks between two scripts, optimized for their respective operating systems, ensures robust performance and enhances the overall security and reliability of the data handling process.
--- linux_bacnet_app/app.py
+++ linux_bacnet_app/app.py
@@ -10,6 +10,7 @@ import asyncio
 import logging
 import aiofiles
 from datetime import datetime
+import psutil
 
 from points import *
 
@@ -17,16 +18,16 @@ from points import *
 # $ python3 app.py --name Slipstream --instance 3056672 --address 10.7.6.201/24:47820
 
 
-logging.basicConfig(level=logging.INFO)
+logging.basicConfig(level=logging.INFO)  
 
 _debug = 0
 
-
 class ReaderApp:
     def __init__(self, args, app_status):
+
         self.current_file_date = datetime.today().date()
         self.lock = asyncio.Lock()
-
+        
         super().__init__()
 
         # embed the bacpypes BACnet application
@@ -34,13 +35,16 @@ class ReaderApp:
         self.app_status = app_status
         self.app.add_object(app_status)
         self.app_state = "active"
-
+        
         # Start the openleadr client
         asyncio.create_task(self.dataset_maker())
 
         # create a task to update the values
         asyncio.create_task(self.update_bacnet_server_values())
-
+        
+        # Add the system metrics logging task
+        asyncio.create_task(self.log_system_metrics())
+        
     async def share_data_to_bacnet_server(self):
         # BACnet server processes
         # not used at the moment but could be if some external
@@ -55,7 +59,7 @@ class ReaderApp:
             await asyncio.sleep(1)
             self.app_status.presentValue = await self.share_data_to_bacnet_server()
 
-    async def write_to_csv(self, data, time):
+    async def write_to_csv(self, data, is_system_metrics=False):
         current_date = datetime.today().date()
 
         # Create the data and archived directories if they don't exist
@@ -68,12 +72,8 @@ class ReaderApp:
             # Rename and move the existing file to the archived directory
             existing_file_path = os.path.join(data_dir, "bacnet_data.csv")
             if os.path.exists(existing_file_path):
-                archived_file_name = (
-                    f"bacnet_data_{self.current_file_date.strftime('%Y_%m_%d')}.csv"
-                )
-                archived_file_path = os.path.join(
-                    archived_directory, archived_file_name
-                )
+                archived_file_name = f"bacnet_data_{self.current_file_date.strftime('%Y_%m_%d')}.csv"
+                archived_file_path = os.path.join(archived_directory, archived_file_name)
                 os.rename(existing_file_path, archived_file_path)
 
             # Update the current file date
@@ -85,20 +85,17 @@ class ReaderApp:
         # Thread safety attempt with asyncio Lock
         async with self.lock:
             # Check if the file exists and has content
-            file_exists = (
-                os.path.exists(csv_file_path) and os.path.getsize(csv_file_path) > 0
-            )
+            file_exists = os.path.exists(csv_file_path) and os.path.getsize(csv_file_path) > 0
 
             async with aiofiles.open(csv_file_path, mode="a", newline="") as file:
-                # Write the headers if the file is new
-                if not file_exists:
+                if not file_exists and not is_system_metrics:
                     await file.write(",".join(["Time", "Point Name", "Value"]) + "\n")
 
-                # Write the scraped data
-                for value, point_name in data:
-                    await file.write(",".join([time, point_name, str(value)]) + "\n")
+                for timestamp, metric_name, metric_value in data:
+                    await file.write(",".join([timestamp, metric_name, str(metric_value)]) + "\n")
 
             logging.info("CSV WRITER DONE!!!")
+
 
     async def do_read_property_task(self, requests):
         read_values = []
@@ -107,13 +104,11 @@ class ReaderApp:
             try:
                 # Destructure the request into its components
                 address, object_id, prop_id, array_index, point_name = request
-
+                
                 logging.info(f" {address} {point_name} GO!")
-
+                
                 if _debug:
-                    logging.info(
-                        f" {address} \n {object_id} \n {prop_id} \n {array_index} \n {point_name}"
-                    )
+                    logging.info(f" {address} \n {object_id} \n {prop_id} \n {array_index} \n {point_name}")
 
                 # Perform the BACnet read property operation
                 value = await self.app.read_property(
@@ -137,34 +132,49 @@ class ReaderApp:
         return read_values
 
     async def scrape_device(self, device_name, device_config):
-        device_address = device_config["address"]
-        scrape_interval = device_config["scrape_interval"]
+        device_address = device_config['address']
+        scrape_interval = device_config['scrape_interval']
         device_requests = [
-            (
-                device_address,
-                point_obj,
-                BACNET_PRESENT_VALUE_PROP_IDENTIFIER,
-                BACNET_PROPERTY_ARRAY_INDEX,
-                f"{device_name}_{point_name.lower()}",
-            )
-            for point_name, point_obj in device_config["points"]
+            (device_address, point_obj, BACNET_PRESENT_VALUE_PROP_IDENTIFIER, 
+            BACNET_PROPERTY_ARRAY_INDEX, f"{device_name}_{point_name.lower()}")
+            for point_name, point_obj in device_config['points']
         ]
 
         while True:
             data = await self.do_read_property_task(device_requests)
-            time = datetime.now().isoformat()
-            logging.info(f"time: {time}, data: {data}")
-            await self.write_to_csv(data, time)
+            timestamp = datetime.now().isoformat()
+            # Update data to include timestamp in each tuple
+            data_with_timestamp = [(timestamp, point_name, value) for value, point_name in data]
+            logging.info(f"time: {timestamp}, data: {data_with_timestamp}")
+            await self.write_to_csv(data_with_timestamp, is_system_metrics=False)
             await asyncio.sleep(scrape_interval)
+
 
     async def dataset_maker(self):
         tasks = []
         for device, config in devices.items():
             task = asyncio.create_task(self.scrape_device(device, config))
             tasks.append(task)
-
+        
         await asyncio.gather(*tasks)
 
+    async def log_system_metrics(self):
+        while True:
+            timestamp = datetime.now().isoformat()
+            cpu_usage = psutil.cpu_percent(interval=1)
+            memory_usage = psutil.virtual_memory().percent
+            disk_usage = psutil.disk_usage('/').percent
+            # Add more metrics if needed
+
+            system_metrics = [
+                (timestamp, 'CPU Usage', cpu_usage),
+                (timestamp, 'Memory Usage', memory_usage),
+                (timestamp, 'Disk Usage', disk_usage),
+                # Add more metrics here
+            ]
+
+            await self.write_to_csv(system_metrics, is_system_metrics=True)
+            await asyncio.sleep(600)
 
 async def main():
     args = SimpleArgumentParser().parse_args()
@@ -188,7 +198,6 @@ async def main():
         logging.debug("app: %r", app)
 
     await asyncio.Future()
-
 
 if __name__ == "__main__":
     try:
